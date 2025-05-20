fcb66a7c2b93bd45c7b90e82c3b5314923609dc5
Saurabhhhh4 <saurabhkumar2504@gmail.com>
2025-05-20 20:55:21 +0530

    dskjh

--- src/components/saur.jsx
+++ src/components/saur.jsx
@@ -17,3 +17,21 @@ people.forEach((person) => {
 });
 
 console.log(grouped);
+
+const people = [
+  { name: "Alice", age: 25 },
+  { name: "Bob", age: 30 },
+  { name: "Charlie", age: 25 },
+  { name: "David", age: 30 },
+];
+
+const grouped = {};
+
+people.forEach((person) => {
+  if (!grouped[person.age]) {
+    grouped[person.age] = [];
+  }
+  grouped[person.age].push(person.name);
+});
+
+console.log(grouped);
