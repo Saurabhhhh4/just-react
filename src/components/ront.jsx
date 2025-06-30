c98318d73747c36f6158a3752a1c3538060d84b2
Saurabhhhh4 <saurabhkumar2504@gmail.com>
2025-06-30 20:21:41 +0530

    another method

--- src/components/ront.jsx
+++ src/components/ront.jsx
@@ -6,19 +6,48 @@ function Login() {
   const [password, setPassword] = useState("");
   const [token, setToken] = useState("");
 
+  // const handleLogin = async () => {
+  //   try {
+  //     const res = await axios.post("http://localhost:5000/login", {
+  //       email,
+  //       password,
+  //     });
+  //     setToken(res.data.token);
+  //     localStorage.setItem("token", res.data.token);
+  //     alert("Login Success!");
+  //   } catch (err) {
+  //     alert("Login Failed");
+  //   }
+  // };
+
+
   const handleLogin = async () => {
-    try {
-      const res = await axios.post("http://localhost:5000/login", {
-        email,
-        password,
-      });
-      setToken(res.data.token);
-      localStorage.setItem("token", res.data.token);
-      alert("Login Success!");
-    } catch (err) {
-      alert("Login Failed");
+  if (!email || !password) {
+    alert("Please enter both email and password");
+    return;
+  }
+
+  try {
+    const response = await axios.post("http://localhost:5000/login", {
+      email,
+      password,
+    });
+
+    const { token } = response.data;
+
+    if (token) {
+      setToken(token);
+      localStorage.setItem("token", token);
+      alert("Login successful!");
+    } else {
+      alert("Invalid response from server.");
     }
-  };
+  } catch (error) {
+    console.error("Login error:", error);
+
+    if (error.response) {
+      // Server
+
 
   return (
     <div>
